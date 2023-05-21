570ab9cb8c888f98655825c9c88a20d5bfb2b776
le_cossg <gaetan.le_cossec@utt.fr>
2023-05-21 17:17:19 +0200

    refactorisation of store.js method updateDiscounts to make it  readable and comprehensible

--- store.js
+++ store.js
@@ -12,55 +12,56 @@ export class Store {
   }
   updateDiscounts() {
     for (var i = 0; i < this.discountOffers.length; i++) {
-      if (
-        this.discountOffers[i].partnerName != "Naturalia" &&
-        this.discountOffers[i].partnerName != "Vinted"
-      ) {
-        if (this.discountOffers[i].discountInPercent > 0) {
-          if (this.discountOffers[i].partnerName != "Ilek") {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
+      switch (this.discountOffers[i].partnerName){
+        case "Naturalia" :
+          this.discountOffers[i].expiresIn-=1;
+          // If already expired and discount will not go over 50 :
+          if (this.discountOffers[i].expiresIn<0 && this.discountOffers[i].discountInPercent<=48){
+            // fast increase
+            this.discountOffers[i].discountInPercent+=2;
           }
-        }
-      } else {
-        if (this.discountOffers[i].discountInPercent < 50) {
-          this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-          if (this.discountOffers[i].partnerName == "Vinted") {
-            if (this.discountOffers[i].expiresIn < 11) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-            if (this.discountOffers[i].expiresIn < 6) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-              }
-            }
+          // If not expired and discount will not go over 50 :
+          else if (this.discountOffers[i].discountInPercent<=49){
+            //slow decrease
+            this.discountOffers[i].discountInPercent+=1;
           }
-        }
-      }
-      if (this.discountOffers[i].partnerName != "Ilek") {
-        this.discountOffers[i].expiresIn = this.discountOffers[i].expiresIn - 1;
-      }
-      if (this.discountOffers[i].expiresIn < 0) {
-        if (this.discountOffers[i].partnerName != "Naturalia") {
-          if (this.discountOffers[i].partnerName != "Vinted") {
-            if (this.discountOffers[i].discountInPercent > 0) {
-              if (this.discountOffers[i].partnerName != "Ilek") {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
-              }
-            }
-          } else {
-            this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent - this.discountOffers[i].discountInPercent;
+          break;
+        case "Vinted" :
+          this.discountOffers[i].expiresIn-=1;
+          // If expired then no discount
+          if (this.discountOffers[i].expiresIn<0){
+            this.discountOffers[i].discountInPercent=0;
           }
-        } else {
-          if (this.discountOffers[i].discountInPercent < 50) {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
+          // If 5 days left and will not go over 50 then increase by 3
+          else if(this.discountOffers[i].expiresIn<=5 && this.discountOffers[i].discountInPercent<=47){
+            this.discountOffers[i].discountInPercent+=3;
           }
-        }
+          // If 10 days left and will not go over 50 then increase by 2
+          else if(this.discountOffers[i].expiresIn<=10 && this.discountOffers[i].discountInPercent<=48){
+            this.discountOffers[i].discountInPercent+=2;
+          }
+          // If more than 10 days left and will not go over 50 then increase by 1
+          else if(this.discountOffers[i].discountInPercent<=49){
+            this.discountOffers[i].discountInPercent+=1;
+          }
+          break;
+        case "Ilek" :
+          // Nothing changes
+          break;
+        default :
+          this.discountOffers[i].expiresIn-=1;
+          // If already expired and discount will not go under 0 :
+          if (this.discountOffers[i].expiresIn<0 && this.discountOffers[i].discountInPercent>=2){
+            // fast decrease
+            this.discountOffers[i].discountInPercent-=2;
+          }
+          // If not expired and discount will not go under 0 :
+          else if (this.discountOffers[i].discountInPercent>=1){
+            //slow decrease
+            this.discountOffers[i].discountInPercent-=1;
+          }
       }
     }
-
     return this.discountOffers;
   }
 }
